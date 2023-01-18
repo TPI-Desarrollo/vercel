77467f0e7162b7c5e517e0f0f88ef676c89d93c7
Chranium <mamedellinr@unal.edu.co>
2023-01-18 15:39:48 -0500

    project restructuring

--- components/ova/navbar/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import animation from './animation';
-
-const navbar = () => {
-   return (<object id="ovaNavBar" onLoad={animation} type="image/svg+xml" data="/image/ovas/template/ovaNavBar.svg"></object>);
-}
-
-export default navbar;
